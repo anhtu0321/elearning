4edb698e0b99189f720346448b29ee6689d72c50
Hoang Van Tu <vantu9486@gmail.com>
2021-07-09 16:39:56 +0700

    hoang van tu

--- resources/views/layouts/admin_footer.blade.php
+++ resources/views/layouts/admin_footer.blade.php
@@ -1,33 +1,8 @@
-<!-- Main Footer -->
 <footer class="main-footer">
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.1.0
     </div>
-  </footer>
-</div>
+</footer>
 
-<script type="application/javascript" src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-
-<script type="application/javascript" src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-<script type="application/javascript" src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-
-<script type="application/javascript" src="{{ asset('dist/js/adminlte.js') }}"></script>
-<script type="application/javascript" src="{{ asset('js/app.js') }}"></script>
-
-
-<script type="application/javascript" src="{{ asset('plugins/jquery-mousewheel/jquery.mousewheel.js') }}"></script>
-<script type="application/javascript" src="{{ asset('plugins/raphael/raphael.min.js') }}"></script>
-<script type="application/javascript"src="{{ asset('plugins/jquery-mapael/jquery.mapael.min.js') }}"></script>
-<script type="application/javascript" src="{{ asset('plugins/jquery-mapael/maps/usa_states.min.js') }}"></script>
-
-<script type="application/javascript" src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
-
-
-<script type="application/javascript" src="{{ asset('dist/js/demo.js') }}"></script>
-
-<script type="application/javascript" src="{{ asset('dist/js/pages/dashboard2.js') }}"></script>
-</body>
-</html>
--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -1,6 +1,51 @@
-@include('layouts.admin_header')
-@include('layouts.admin_sidebar')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<admin-master></admin-master>
+  <title>AdminLTE 3 | Dashboard 2</title>
 
-@include('layouts.admin_footer')
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+</head>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper" id="app">
+
+    @include('layouts.admin_header')
+    @include('layouts.admin_sidebar')
+
+        <admin-master></admin-master>  
+    </div>
+    @include('layouts.admin_footer')
+
+</div>
+
+    <script type="application/javascript" src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+
+    <script type="application/javascript" src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script type="application/javascript" src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    
+
+
+    <script type="application/javascript" src="{{ asset('plugins/jquery-mousewheel/jquery.mousewheel.js') }}"></script>
+    <script type="application/javascript" src="{{ asset('plugins/raphael/raphael.min.js') }}"></script>
+    <script type="application/javascript"src="{{ asset('plugins/jquery-mapael/jquery.mapael.min.js') }}"></script>
+    <script type="application/javascript" src="{{ asset('plugins/jquery-mapael/maps/usa_states.min.js') }}"></script>
+    <script type="application/javascript" src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+    <script type="application/javascript" src="{{ asset('dist/js/adminlte.js') }}"></script>
+    <script type="application/javascript" src="{{ asset('dist/js/demo.js') }}"></script>
+
+    {{-- <script type="application/javascript" src="{{ asset('dist/js/pages/dashboard2.js') }}"></script> --}}
+    <script type="application/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
+
